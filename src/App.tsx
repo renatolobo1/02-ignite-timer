c1c8b8b78eb8487c979bc012d5829ca5c301ec27
Renato Lobo <renatolobo_@hotmail.com>
2022-07-28 21:12:20 -0300

    styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Button } from "./components/Button";
+
 export function App() {
-  
+
   return (
-    <div>teste</div>
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button  variant="danger" />
+      <Button />
+    </>
   )
 }
 
